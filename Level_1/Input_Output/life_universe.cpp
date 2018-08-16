ebdf03691cabb2f2f2a177f77d227a73b7b7372d
BullHacks3 <333bulldog@gmail.com>
2018-08-16 13:51:13 +0530

    Life,Universe,and Everything

---
+++ Level_1/Input_Output/life_universe.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+  while(1)
+  {
+  int k;
+  cin>>k;
+   if(k!=42)
+     cout<<k<<"\n";
+   else
+      break;
+  }
+return 0;
+}
